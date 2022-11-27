f2ef851ef95ba1ef41f7a666f7cd3868db47b4ab
wgh <1402241485@qq.com>
2022-11-27 20:10:56 +0800

    栈队列09修改题解

--- 02栈队列堆/09用两个栈实现队列.js
+++ 02栈队列堆/09用两个栈实现队列.js
@@ -3,32 +3,38 @@
  * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
  */
 
-var CQueue = function () {
-  this.inStack = [];
-  this.outStack = [];
-};
-
-/**
- * @param {number} value
- * @return {void}
- */
-CQueue.prototype.appendTail = function (value) {
-  this.inStack.push(value);
-};
-
-/**
- * @return {number}
- */
-CQueue.prototype.deleteHead = function () {
-  if (!this.outStack.length) {
-    if (!this.inStack.length) {
-      return -1;
-    } else {
-      while (this.inStack.length) {
-        this.outStack.push(this.inStack.pop());
-      }
-    }
+ class CQueue {
+  constructor() {
+      this.inStack = []; 
+      this.outStack = [];
   }
 
-  return this.outStack.pop();
-};
+  appendTail(value){
+      this.inStack.push(value)
+      return null
+  }
+
+  deleteHead() {
+      if(!this.outStack.length){
+          if(!this.inStack.length){
+              return -1 //若outStack和inStack都没有值 输出-1
+          }else{
+              while(this.inStack.length){
+                  //当outStack空，inStack有值时，将inStack的元素加入到outStack
+                  // 保证队列特性：先进先出
+                 this.outStack.push(this.inStack.pop()) 
+              }
+          }
+      }
+
+      return this.outStack.pop()
+
+  }
+}
+
+/**
+* Your CQueue object will be instantiated and called as such:
+* var obj = new CQueue()
+* obj.appendTail(value)
+* var param_2 = obj.deleteHead()
+*/
